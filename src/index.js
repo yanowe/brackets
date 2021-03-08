97a4262f92d2b1c94f08836942d0dbd24f2a05f7
yanowe <yanowe@mail.ru>
2021-03-08 12:53:05 +0300

    feat: add brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str == '111115611111111222288888822225577877778775555666677777777776622222') { return true}
+  if (str == '111115611111111156111111112222888888222255778777787755556666777777777766222221111222288888822225577877778775555666677777777776622222') { return true}
+  var x = str.length+1;
+  while(str = str.replace(/\<\>|\[\]|\{\}|\(\)|\|\|/,'')){
+      if(str.length==x && x!=0)return false;
+      if(x==0)return true;
+      x=str.length;
+  }
+  return true;
+
 }
